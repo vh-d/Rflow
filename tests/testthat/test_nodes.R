60f8bca59c05bea58fbccb94b1a3dc0ba9d0cfd4
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2019-02-12 22:02:02 +0100

    more tests

--- tests/testthat/test_nodes.R
+++ tests/testthat/test_nodes.R
@@ -1,34 +1,73 @@
-context("Basic operations on nodes")
 
-test_that("generic nodes", {
+# node --------------------------------------------------------------------
+
+context("Creating generic node object")
+
+test_that("nodes can be initiated", {
   expect_error(Rflow::node$new(),               regexp = "[Mm]issing", info = "initialization requires id or env + name")
   expect_error(Rflow::node$new(name = "node1"), regexp = "[Mm]issing", info = "initialization requires id or env + name")
   expect_error(Rflow::node$new(env  = "env1"),  regexp = "[Mm]issing", info = "initialization requires id or env + name")
 
-  expect_error(node1 <- Rflow::node$new(id = "node1"), regexp = "(folder)|(path)|(dir)", info = "path has to be supplied")
+  node1 <- Rflow::node$new(id = "node1")
+  expect_s3_class(object = node1, class = "node")
+  expect_s3_class(object = node1, class = "R6")
 
+  expect_is(node1$persistence, "list")
+  expect_false(node1$persistence$enabled)
+
+  expect_null(node1$depends)
+  expect_null(node1$trigger_condition)
+  expect_null(node1$last_updated)
+
+  node12 <- Rflow::node$new(name = "node12", env = "env")
+  expect_equal(node12$id, "env.node12", info = "Nodes can be initialize with name/env pair instead of id")
+})
+
+test_that("nodes can be initiated with persistence", {
   tmp_rflow_dir <- tempdir()
-  node2 <- Rflow::node$new(id = "node2", storage = tmp_rflow_dir)
-  expect_s3_class(object = node2, class = "node")
-  expect_s3_class(object = node2, class = "R6")
+  node2 <- Rflow::node$new(id = "node2", persistence = list(path = tmp_rflow_dir), caching = FALSE)
+
+  expect_is(node2$persistence, "list")
+  expect_true(node2$persistence$enabled)
 
 })
 
 
-test_that("r nodes", {
+
+# r_node ------------------------------------------------------------------
+
+
+context("Creating r_node objects")
+
+test_that("nodes can be initiated", {
   expect_error(Rflow::r_node$new(),               regexp = "[Mm]issing", info = "initialization requires id or env + name")
   expect_error(Rflow::r_node$new(name = "node1"), regexp = "[Mm]issing", info = "initialization requires id or env + name")
   expect_error(Rflow::r_node$new(env  = "env1"),  regexp = "[Mm]issing", info = "initialization requires id or env + name")
 
-  expect_error(Rflow::r_node$new(id = "rnode1"), regexp = "(folder)|(path)|(dir)", info = "path has to be supplied")
+  node1 <- Rflow::r_node$new(id = "node1")
+  expect_s3_class(object = node1, class = "r_node")
+  expect_s3_class(object = node1, class = "node")
+  expect_s3_class(object = node1, class = "R6")
 
-  tmp_rflow_dir <- tempdir()
-  expect_error(Rflow::r_node$new(id = "rnode1", storage = tmp_rflow_dir), regexp = "(folder)|(path)|(dir)", info = "either caching = FALSE or path has to be supplied")
-  expect_warning(rnode2 <- Rflow::r_node$new(id = "rnode2", storage = tmp_rflow_dir, caching = FALSE), regexp = "R expression", "initialization without r_code or r_expression should produce warning")
+  expect_is(node1$persistence, "list")
+  expect_false(node1$persistence$enabled)
 
-  expect(exists("rnode2"))
-  expect_s3_class(object = rnode2, class = "r_node")
-  expect_s3_class(object = rnode2, class = "node")
-  expect_s3_class(object = rnode2, class = "R6")
-
+  expect_null(node1$depends)
+  expect_null(node1$trigger_condition)
+  expect_true(is.null(node1$last_updated) || is.na(node1$last_updated))
 })
+
+test_that("nodes can be initiated with persistence", {
+  tmp_rflow_dir <- tempdir()
+  node2 <- Rflow::r_node$new(id = "node2", persistence = list(path = tmp_rflow_dir), caching = FALSE)
+  expect_is(node2$persistence, "list")
+  expect_true(node2$persistence$enabled)
+})
+
+test_that("nodes can be initiated with caching", {
+  tmp_rflow_dir <- tempdir()
+  expect_error(Rflow::r_node$new(id = "node3", caching = TRUE), "caching")
+  node3 <- Rflow::r_node$new(id = "node3", caching = TRUE, cache_store = tmp_rflow_dir)
+  node4 <- Rflow::r_node$new(id = "node3", caching = TRUE, cache_store = tmp_rflow_dir)
+})
+
--- tests/testthat/test_rflow.R
+++ tests/testthat/test_rflow.R
@@ -1,8 +1,36 @@
-context("basic operations on Rflow")
+context("Builidng rflows")
 
 test_that("Rflow can be crated", {
   rf1 <- Rflow::new_rflow()
   expect_true(exists("rf1"), "rflow object exists")
   expect_s3_class(rf1, "rflow")
   expect_s3_class(rf1, "environment")
+
+})
+
+test_that("nodes can be added", {
+  rf2 <- Rflow::new_rflow()
+  expect_true(Rflow::add_node(list(id = "node1"), rflow = rf2, verbose = FALSE))
+  expect_is(rf2[["node1"]], "node")
+
+  expect_true(Rflow::add_node(list(id = "node2", depends = "node1"), rflow = rf2, verbose = FALSE))
+  expect_is(rf2[["node2"]], "node")
+  expect_length(rf2$node2$depends, 1)
+  expect_null(rf2$node2$upstream)
+})
+
+
+test_that("nodes can be connected", {
+  rf2 <- Rflow::new_rflow()
+  expect_true(Rflow::add_node(list(id = "node1"), rflow = rf2, verbose = FALSE))
+  expect_true(Rflow::add_node(list(id = "node2", depends = "node1"), rflow = rf2, verbose = FALSE))
+
+  # connecting nodes
+  connect_nodes(rf2, verbose = FALSE)
+
+  expect_length(rf2$node2$upstream,   1)
+  expect_length(rf2$node1$downstream, 1)
+
+  expect_equal(rf2$node2$upstream[[1]],   rf2$node1)
+  expect_equal(rf2$node1$downstream[[1]], rf2$node2)
 })
