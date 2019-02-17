8476d16293c3b4f66f7140ccb1a2745073d2e380
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2019-02-17 08:13:58 +0100

    more tests

--- tests/testthat/test_nodes.R
+++ tests/testthat/test_nodes.R
@@ -62,10 +62,15 @@ test_that("nodes can be initiated with persistence", {
   node2 <- Rflow::r_node$new(id = "node2", persistence = list(path = tmp_rflow_dir))
   expect_is(node2$persistence, "list")
   expect_true(node2$persistence$enabled)
+  expect_equal(node2$persistence$path, tmp_rflow_dir)
+  expect_equal(node2$persistence$file, "node2.rds")
 })
 
 test_that("nodes can be initiated with caching", {
   tmp_rflow_dir <- tempdir()
   node3 <- Rflow::r_node$new(id = "node3", cache = list(path = tmp_rflow_dir))
+  expect_true(node3$cache$enabled)
+  expect_equal(node3$cache$path, tmp_rflow_dir)
+  expect_equal(node3$cache$file, "node3.rds")
 })
 
