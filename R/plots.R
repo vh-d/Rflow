7490ca4ee91be6dbf0f353ee28b08b0dbbb745fc
vh-d <vencahaus@seznam.cz>
2019-06-26 16:25:21 +0200

    avoid marittr dependency

--- R/plots.R
+++ R/plots.R
@@ -8,9 +8,9 @@ disc_scale <- function(
 
 
 visData <- function(rflow, tags = NULL, includeIsolated = TRUE) {
-
+  
   if (!requireNamespace("data.table", quietly = TRUE)) stop("data.table package required to plot graphs")
-
+  
   dtEDGES <- edges.rflow(rflow)
   dtNODES <- nodes.rflow(rflow)
   
@@ -26,12 +26,12 @@ visData <- function(rflow, tags = NULL, includeIsolated = TRUE) {
     rm(tags)
     dtNODES <- dtNODES[stringr::str_detect(tags, stringr::fixed(paste0("|", query_tags, "|")))]
   }
-
+  
   # drop isolated/lonely nodes if needed
   if (isFALSE(includeIsolated)) {
     dtNODES <- dtNODES[dtEDGES[, .(id = unique(c(from, to)))], on = "id"]
   }
-
+  
   # dtNODES[, label := paste0(env, "\n", name)]
   # dtNODES[,
   #         title :=
@@ -79,22 +79,22 @@ visData <- function(rflow, tags = NULL, includeIsolated = TRUE) {
   #             "accdb_node" = "square",
   #             "file_node"  = "star"
   #           )[node_type]]
-
+  
   # colors are dynamic: based on number of environments
   unique_levels <- dtNODES[, unique(env)]
   unique_levels_colors <- disc_scale()(length(unique_levels))
   names(unique_levels_colors) <- unique_levels
-
+  
   dtNODES[, color.background := unique_levels_colors[env]]
   dtNODES[, color.border     := color.background]
   dtNODES[, color.hover      := color.background]
   dtNODES[, color.highlight.border := "black"]
   dtNODES[, color.highlight.background := color.background]
   # dtNODES[, color.border := "black"]
-
+  
   setkeyv(dtNODES, "id")
   setkeyv(dtEDGES, c("from", "to"))
-
+  
   list(
     dtNODES = dtNODES,
     dtEDGES = dtEDGES
@@ -112,57 +112,69 @@ visData <- function(rflow, tags = NULL, includeIsolated = TRUE) {
 #'
 #' @export
 plot.rflow <- function(rflow, tags = NULL, includeIsolated = TRUE, direction = "LR", ...) {
-
+  
   if (!requireNamespace("visNetwork", quietly = TRUE)) stop("visNetwork package required to plot graphs")
-
+  
   l <- visData(rflow = rflow, tags = tags, includeIsolated = includeIsolated)
-
+  
   visNetwork::visNetwork(
     nodes = l$dtNODES,
     edges = l$dtEDGES,
     ...
-  ) %>%
-    # visLayout()
-    visNetwork::visHierarchicalLayout(
-      sortMethod = "directed",
-      direction = direction
-    ) %>%
-    visNetwork::visNodes(
-      borderWidthSelected = 4
-    ) %>%
-    visNetwork::visEdges(
-      arrows = "to",
-      smooth = FALSE
-    ) %>%
-    visNetwork::visOptions(
-
-      nodesIdSelection =
-        list(
-          enabled = TRUE,
-          useLabels = TRUE
-        ),
-
-      selectedBy =
-        list(
-          variable = "env"
-        ),
-
-      highlightNearest =
-        list(
-          enabled = TRUE,
-          algorithm = "hierarchical",
-          degree = list(from = 50, to = 50),
-          hover   = TRUE
-        )
-
-    ) %>%
-    visNetwork::visInteraction(
-      tooltipDelay      = 1,
-      navigationButtons = TRUE,
-      dragNodes         = TRUE) %>%
-    visNetwork::visPhysics(
-      enabled = FALSE
-    )
+  ) ->.
+  
+  # visLayout()
+  visNetwork::visHierarchicalLayout(
+    .,
+    sortMethod = "directed",
+    direction = direction
+  ) ->.
+  
+  visNetwork::visNodes(
+    .,
+    borderWidthSelected = 4
+  ) ->.
+  
+  visNetwork::visEdges(
+    .,
+    arrows = "to",
+    smooth = FALSE
+  ) ->.
+  
+  visNetwork::visOptions(
+    .,
+    nodesIdSelection =
+      list(
+        enabled = TRUE,
+        useLabels = TRUE
+      ),
+    
+    selectedBy =
+      list(
+        variable = "env"
+      ),
+    
+    highlightNearest =
+      list(
+        enabled = TRUE,
+        algorithm = "hierarchical",
+        degree = list(from = 50, to = 50),
+        hover   = TRUE
+      )
+    
+  ) ->.
+  
+  visNetwork::visInteraction(
+    .,
+    tooltipDelay      = 1,
+    navigationButtons = TRUE,
+    dragNodes         = TRUE
+  ) ->.
+  
+  visNetwork::visPhysics(
+    .,
+    enabled = FALSE
+  )
 }
 
 
@@ -179,65 +191,77 @@ plot.rflow <- function(rflow, tags = NULL, includeIsolated = TRUE, direction = "
 #'
 #' @examples
 visRflow <- function(rflow, tags = NULL, includeIsolated = TRUE, direction = "UD", orderBy = "id", ...) {
-
+  
   if (!requireNamespace("visNetwork", quietly = TRUE)) stop("visNetwork package required to plot graphs")
-
+  
   l <- visData(rflow = rflow, tags = tags, includeIsolated = includeIsolated)
-
+  
   nodes <- l$dtNODES
   edges <- l$dtEDGES
-
+  
   if (length(orderBy)) setorderv(nodes, orderBy)
-
+  
   visNetwork::visNetwork(
     nodes = nodes,
     edges = edges,
     ...
-  ) %>%
-    visNetwork::visIgraphLayout(
-      layout  = "layout_with_sugiyama", # layout with layers
-      physics = FALSE,
-      smooth  = TRUE,
-      type    = "full"
-    ) %>%
-    visNetwork::visNodes(
-      borderWidthSelected = 4
-    ) %>%
-    visNetwork::visEdges(
-      arrows = "to",
-      smooth = FALSE
-    ) %>%
-    visNetwork::visOptions(
-
-      nodesIdSelection =
-        list(
-          enabled = TRUE,
-          useLabels = TRUE
-        ),
-
-      selectedBy =
-        list(
-          variable = "env"
-        ),
-
-      highlightNearest =
-        list(
-          enabled = TRUE,
-          algorithm = "hierarchical",
-          degree = list(from = 50, to = 50),
-          hover   = TRUE
-        ),
-
-      autoResize = TRUE,
-
-    ) %>%
-    visNetwork::visInteraction(
-      tooltipDelay      = 1,
-      navigationButtons = TRUE,
-      dragNodes         = TRUE) %>%
-    visNetwork::visPhysics(
-      enabled = FALSE
-    )
+  ) ->.
+  
+  visNetwork::visIgraphLayout(
+    .,
+    layout  = "layout_with_sugiyama", # layout with layers
+    physics = FALSE,
+    smooth  = TRUE,
+    type    = "full"
+  ) ->.
+  
+  visNetwork::visNodes(
+    .,
+    borderWidthSelected = 4
+  ) ->.
+  
+  visNetwork::visEdges(
+    .,
+    arrows = "to",
+    smooth = FALSE
+  ) ->.
+  
+  visNetwork::visOptions(
+    .,
+    nodesIdSelection =
+      list(
+        enabled = TRUE,
+        useLabels = TRUE
+      ),
+    
+    selectedBy =
+      list(
+        variable = "env"
+      ),
+    
+    highlightNearest =
+      list(
+        enabled = TRUE,
+        algorithm = "hierarchical",
+        degree = list(from = 50, to = 50),
+        hover   = TRUE
+      ),
+    
+    autoResize = TRUE,
+    
+  ) ->.
+  
+  visNetwork::visInteraction(
+    .,
+    tooltipDelay      = 1,
+    navigationButtons = TRUE,
+    dragNodes         = TRUE
+  ) ->.
+  
+  visNetwork::visPhysics(
+    .,
+    enabled = FALSE
+  )
 }
 
 
@@ -253,65 +277,78 @@ visRflow <- function(rflow, tags = NULL, includeIsolated = TRUE, direction = "UD
 #'
 #' @examples
 visRflow2 <- function(rflow, tags = NULL, includeIsolated = TRUE, direction = "UD", orderBy = "id", ...) {
-
+  
   if (!requireNamespace("visNetwork", quietly = TRUE)) stop("visNetwork package required to plot graphs")
-
+  
   l <- visData(rflow = rflow, tags = tags, includeIsolated = includeIsolated)
-
+  
   nodes  <- l$dtNODES
   edges  <- l$dtEDGES
-
+  
   if (length(orderBy)) setorderv(nodes, orderBy)
-
+  
   visNetwork::visNetwork(
     nodes = nodes,
     edges = edges,
     ...
-  ) %>%
-    visNetwork::visHierarchicalLayout(
-      sortMethod = "directed",
-      direction  = direction,
-    ) %>%
-    visNetwork::visNodes(
-      borderWidthSelected = 4
-    ) %>%
-    visNetwork::visEdges(
-      arrows = "to",
-      smooth = TRUE
-    ) %>%
-    visNetwork::visOptions(
-
-      nodesIdSelection =
-        list(
-          enabled = TRUE,
-          useLabels = TRUE
-        ),
-
-      selectedBy =
-        list(
-          variable = "env"
-        ),
-
-      highlightNearest =
-        list(
-          enabled = TRUE,
-          algorithm = "hierarchical",
-          degree = list(from = 50, to = 50),
-          hover   = TRUE
-        ),
-
-      autoResize = TRUE
-
-    ) %>%
-    visNetwork::visInteraction(
-      tooltipDelay      = 1,
-      navigationButtons = TRUE,
-      dragNodes         = TRUE) %>%
-    visNetwork::visPhysics(
-      enabled = TRUE,
-      stabilization = FALSE,
-      hierarchicalRepulsion = list(
-        nodeDistance = 200
-      )
+    
+  ) ->.
+  
+  visNetwork::visHierarchicalLayout(
+    .,
+    sortMethod = "directed",
+    direction  = direction,
+  ) ->.
+  
+  visNetwork::visNodes(
+    .,
+    borderWidthSelected = 4
+  ) ->.
+  
+  visNetwork::visEdges(
+    .,
+    arrows = "to",
+    smooth = TRUE
+  ) ->.
+  
+  visNetwork::visOptions(
+    .,
+    nodesIdSelection =
+      list(
+        enabled = TRUE,
+        useLabels = TRUE
+      ),
+    
+    selectedBy =
+      list(
+        variable = "env"
+      ),
+    
+    highlightNearest =
+      list(
+        enabled = TRUE,
+        algorithm = "hierarchical",
+        degree = list(from = 50, to = 50),
+        hover   = TRUE
+      ),
+    
+    autoResize = TRUE
+    
+  ) ->.
+  
+  visNetwork::visInteraction(
+    .,
+    tooltipDelay      = 1,
+    navigationButtons = TRUE,
+    dragNodes         = TRUE
+  ) ->.
+  
+  visNetwork::visPhysics(
+    .,
+    enabled = TRUE,
+    stabilization = FALSE,
+    hierarchicalRepulsion = list(
+      nodeDistance = 200
     )
+  )
 }
