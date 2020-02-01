8f51053416fe3214e0153ce6b3b8dbae5fad8205
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2020-02-01 15:08:53 +0100

    Do not import RcppTOML package (only suggest).

--- R/toml.R
+++ R/toml.R
@@ -1,5 +1,8 @@
 #' @export
 read_TOML_file <- function(file, escape = FALSE, ...) {
+
+  if (!requireNamespace("RcppTOML")) stop("Package RcppTOML not available!")
+
   tryCatch(
     RcppTOML::parseTOML(input = file, escape = escape, ...),
     error = function(e) {
