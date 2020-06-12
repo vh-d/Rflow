0ab8aca06c6c578b2caa7f09550290f58764151c
Vaclav Hausenblas <vaclav.hausenblas@gmail.com>
2020-06-12 22:16:49 +0200

    Add S3 method for indexing node_list which preserves class attribute.

--- R/node_primitives.R
+++ R/node_primitives.R
@@ -235,15 +235,22 @@ depends.node <- function(x, inverse = FALSE, results = character(), ...) {
   }
 }
 
+
+`[.node_list` <- function(x, ...) {
+  structure(NextMethod("[", x), class = c("node_list", "list"))
+}
+
 #' @export
 c.node_list <- function(...) {
-  structure(c(...), class = "node_list")
+  structure(c(...), class = c("node_list", "list"))
 }
 
 #' @export
 print.node_list <- function(x, ...) {
+  cat("<list of nodes>\n")
   print(get_id(x), ...)
 }
+
 
 #' @export
 as.list.node_list <- function(x) {
